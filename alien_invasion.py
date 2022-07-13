6b2503e9952bbf888be5429065ffcf0c13e7daae
MRA-AK <mra1373@gmail.com>
2022-07-13 12:09:53 +0430

    Refactoring the AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,25 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            # Draw the ship on the screen
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            # Update screen
+            self._update_screen()
+        
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        # Draw the ship on the screen
+        self.ship.blitme()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
