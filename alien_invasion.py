b48872ca14ba6725724b4b6d4210b794a9683e79
MRA-AK <mra1373@gmail.com>
2022-07-13 14:42:17 +0430

    Move ship to left and right based on keypress

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,9 @@ class AlienInvasion:
             # Watch for keyboard and mouse events
             self._check_events()
 
+            # Handle ship movement
+            self.ship.update()
+
             # Update screen
             self._update_screen()
         
@@ -35,6 +38,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
